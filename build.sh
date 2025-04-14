e24bda0f115e80feab3e866fb9e7415fa81af7da
defowler2005 <defowler2005@defowler2005-HP-Pavilion-15-Notebook-PC>
2025-04-14 21:51:09 +0930

    bafalcon

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 echo "Started building server binary...\n";
 
+#sudo apt update
+#sudo apt install -y libssl-dev
+
 g++ -std=c++20 ./main.cpp -o server # -lssl -lcrypto
 
 if [ $? -eq 0 ]; then echo "Nice the binary was successfully built!";
