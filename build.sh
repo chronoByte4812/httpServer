f5ba2f4e128f75aad2e27742309af2d87b3464c7
defowler2005 <defowler2005@defowler2005-Modern-14-F13MG>
2025-04-16 20:53:53 +0930

    A77

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
-echo "Started building server binary...\n";
+echo "Started building binary...\n";
 
-#sudo apt update
-#sudo apt install -y libssl-dev
+# sudo apt update
+# sudo apt install -y libssl-dev
 
 g++ -std=c++20 ./main.cpp -o server # -lssl -lcrypto
 
 if [ $? -eq 0 ]; then echo "Nice the binary was successfully built!";
-else echo "\nBinary failed to build binary";
+else echo "\nBinary failed to build!";
+fi
+
+if [[ "$1" == "--runAfterBuild" ]]; then
+    echo "--runAfterBuild flag! the binary has started";
+    ./server
 fi
